79d0b9fe7da006dbadacb57075bbb004110dd68a
Mukesh Kumar <78642261+mukesh-9@users.noreply.github.com>
2024-04-17 14:47:22 +0530

    Create Weather Observation Station 1.sql

--- Weather Observation Station 1.sql
+++ Weather Observation Station 1.sql
@@ -2,15 +2,15 @@ Question:-
 ****************
 Query a list of CITY and STATE from the STATION table.
 The STATION table is described as follows:
--------------------------
-Field - Type
--------------------------
-ID - NUMBER
-CITY - VARCHAR2(21)
-STATE - VARCHAR2(2)
-LAT_N - NUMBER
-LONG_W - NUMBER
--------------------------
++-------------+---------------------+
+| Field       |   Type              |
++-------------+---------------------+
+| ID          | NUMBER              |
+| CITY        | VARCHAR(21)         |
+| STATE       | VARCHAR(2)          |
+| LAT_N       | NUMBER              |
+| LONG_W      | NUMBER              |
++-------------+---------------------+
 Where LAT_N is the northern latitude and LONG_W is the western longitude.
 
 Solution:-
