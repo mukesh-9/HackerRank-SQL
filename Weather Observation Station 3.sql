141b3d4799ddec99a2773e58c68ed91ec13879a2
Mukesh Kumar <78642261+mukesh-9@users.noreply.github.com>
2024-04-17 14:48:14 +0530

    Create Weather Observation Station 3.sql

--- Weather Observation Station 3.sql
+++ Weather Observation Station 3.sql
@@ -2,15 +2,17 @@ Question:-
 ****************
 Query a list of CITY names from STATION for cities that have an even ID number. Print the results in any order, but exclude duplicates from the answer.
 The STATION table is described as follows:
--------------------------
-Field - Type
--------------------------
-ID - NUMBER
-CITY - VARCHAR2(21)
-STATE - VARCHAR2(2)
-LAT_N - NUMBER
-LONG_W - NUMBER
--------------------------
+
++-------------+---------------------+
+| Field       |   Type              |
++-------------+---------------------+
+| ID          | NUMBER              |
+| CITY        | VARCHAR(21)         |
+| STATE       | VARCHAR(2)          |
+| LAT_N       | NUMBER              |
+| LONG_W      | NUMBER              |
++-------------+---------------------+
+
 Where LAT_N is the northern latitude and LONG_W is the western longitude.
 
 Solution:-
