7d7b1716c27b0195f8c2cdb552c1d60f392a8938
Mukesh Kumar <78642261+mukesh-9@users.noreply.github.com>
2024-04-18 11:31:38 +0530

    Create Weather Observation Station 12.sql

---
+++ Weather Observation Station 12.sql
@@ -0,0 +1,22 @@
+Question:-
+****************
+Query the list of CITY names from STATION that either do not start with vowels or do not end with vowels. Your result cannot contain duplicates.
+The STATION table is described as follows:
+
++-------------+---------------------+
+| Field       |   Type              |
++-------------+---------------------+
+| ID          | NUMBER              |
+| CITY        | VARCHAR(21)         |
+| STATE       | VARCHAR(2)          |
+| LAT_N       | NUMBER              |
+| LONG_W      | NUMBER              |
++-------------+---------------------+
+
+where LAT_N is the northern latitude and LONG_W is the western longitude.
+
+Solution:-
+****************
+SELECT DISTINCT CITY FROM STATION WHERE CITY NOT REGEXP '^[aeiouAEIOU]' AND CITY NOT REGEXP '[aeiouAEIOU]$'
+
+Explain:- This is same as "Weather Observation Station 11". Here, we just need replace the 'OR' operator with 'AND' operator. Because now we have to get city names those are not starting with a vowel and not end wth a vowel as well.
