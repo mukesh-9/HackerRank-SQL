6b85561072f923d10449640aaef3d7d22505eaa3
Mukesh Kumar <78642261+mukesh-9@users.noreply.github.com>
2024-04-18 15:25:55 +0530

    Create Employee Names.sql

---
+++ Employee Names.sql
@@ -0,0 +1,23 @@
+Question:-
+****************
+Write a query that prints a list of employee names (i.e.: the name attribute) from the Employee table in alphabetical order.
+The Employee table containing employee data for a company is described as follows:
+  
++----------------+---------------------+
+| Column         |   Type              |
++----------------+---------------------+
+| employee_id    | Integer             |
+| name           | String              |
+| months         | Integer             |
+| salary         | Integer             |
++----------------+---------------------+
+  
+where employee_id is an employee's ID number, name is their name, months is the total number of months they've been working for the company, and salary is their monthly salary.
+
+
+Solution:-
+****************
+SELECT name FROM Employee ORDER BY name;
+
+Explain:- Here, we just need to get the name from the table Employee with the order by 'name' in asending. 
+          We don't need to mention the order when it's in acending because the data is sorting in ascending as default.
