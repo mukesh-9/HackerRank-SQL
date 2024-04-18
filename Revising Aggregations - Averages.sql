8854e24899ee74dc36e9c2bfe3865306fafde1cd
Mukesh Kumar <78642261+mukesh-9@users.noreply.github.com>
2024-04-18 16:20:30 +0530

    Create Revising Aggregations - Averages.sql

---
+++ Revising Aggregations - Averages.sql
@@ -0,0 +1,19 @@
+Question:-
+****************
+Query the average population of all cities in CITY where District is California.
+The CITY table is described as follows:
++----------------+---------------------+
+| Field          |   Type              |
++----------------+---------------------+
+| ID             | NUMBER              |
+| NAME           | VARCHAR(17)         |
+| COUNTRYCODE    | VARCHAR(3)          |
+| DISTRICT       | VARCHAR2(20)        |
+| POPULATION     | NUMBER              |
++----------------+---------------------+
+
+Solution:-
+****************
+SELECT AVG(POPULATION) FROM CITY WHERE DISTRICT='California';
+
+Explain:- We need to get the avaerage population of all cities those district is 'California'.
